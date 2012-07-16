b26701c0ef553b83885ba498669c20a7320c0c0c
David Millett <dbmillett@gmail.com>
2012-07-15 21:16:16 -0500

    fixing git prompt at depth

--- git-simple-prompt.sh
+++ git-simple-prompt.sh
@@ -1,7 +1,55 @@
 #!/bin/bash
 
-SHOW_REPO="true"
-SHOW_SUGGESTION="true"
+export SHOW_REPO="true"
+export SHOW_SUGGESTION="true"
+
+#
+# Whether or not to show the repo name in the git prompt
+# expects path $HOME/bash/bash-help/
+function git_prompt_toggle_repo() {
+
+  if [[ "$SHOW_REPO" == "true" ]]; then
+    echo "Setting SHOW_REPO to 'false'"
+    export $SHOW_REPO="false"
+  else
+    echo "Setting SHOW_REPO to 'true'"
+    export $SHOW_REPO="true"
+  fi
+}
+
+#
+# Whether or not to show suggested git actions
+function git_prompt_toggle_suggestions() {
+
+  if [[ "$SHOW_SUGGESTION" == "true" ]]; then
+    echo "Setting SHOW_SUGGESTION to 'false'"
+    export $SHOW_SUGGESTION="false"
+  else
+    echo "Setting SHOW_SUGGESTION to 'true'"
+    export $SHOW_SUGGESTION="true"
+  fi
+}
+
+#
+# Internally build suggestion for next git action
+# These suggestions are comma delimited inside parenthessi
+function build_git_suggestion() {
+  
+  if [[ "$SHOW_SUGGESTION" == "true" ]] ; then
+    message=""
+    for arg in "$@"; do
+      if [[ "$message" == "" ]] ; then
+        message="$arg"
+      else
+        message="$message,$arg"
+      fi
+    done
+    echo "($message)"
+  else
+    echo ""
+  fi
+}
+
 
 # Color options
 # 0: reset
@@ -26,44 +74,21 @@ GREEN_B="\[\e[01;32m\]"
 BLUE="\[\e[00;34m\]"
 BLUE_B="\[\e[01;34m\]"
 
-# Commit changes locally
-ARROW_UP="↑"
-# Pull changes from remote (todo)
-ARROW_DOWN="↓"
+# Suggestions
+PUSH="↑"
+FETCH="↓"
+MERGE="↓↑" 
+MODIFIED="*"
+EXTERNAL="+"
 
 # GIT regex
-BRANCH_PATTERN="^# On branch ([a-zA-Z0-9]*)"
+BRANCH_PATTERN="# On branch ([a-zA-Z0-9]*)"
 CHANGED_PATTERN="# Changed but not updated:"
 MODIFIED_PATTERN="# Changes not staged for commit:"
 NEW_FILE_PATTERN="# Changes to be committed:"
 UNTRACKED_PATTERN="# Untracked files:"
+HAS_REMOTE_PATTERN="[remote \"(.*)\"]"
 ERROR_PATTERN="fatal: "
-
-#
-# Internally build suggestion for next git action
-# These suggestions are comma delimited inside parenthessi
-function build_git_suggestion() {
-  
-  if [[ "$SHOW_SUGGESTION" == "true" ]] ; then
-    message=""
-    for arg in "$@"; do
-      if [[ "$message" == "" ]] ; then
-        message="$arg"
-      else
-        message="$message,$arg"
-      fi
-    done
-    echo "($message)"
-  else
-    echo ""
-  fi
-}
-
-function git_upstream_diffs_exist() {
-
-  # todo
-
-}
 
 #
 # how-to
@@ -74,52 +99,53 @@ function git_upstream_diffs_exist() {
 # Ready for use
 function git_simple_prompt() {
 
-  # todo handle a PS that is passed to the function
-  ps_default="$1 "
+  #ps_default="$1 "
   pscmd="[\u:\w]$ "
-
-  # It's a git project
-  if [[ -d ".git" ]]; then
+  status=`git status 2>&1`
+  
+  # It's a git project 
+  if [[ $status =~ $BRANCH_PATTERN ]]; then
+    branch="${BASH_REMATCH[1]}"
     project=${PWD##*/}
-    status=`git status`
 
-    branch=""
-    if [[ "$status" =~ "$BRANCH_PATTERN" ]] ; then
-      branch="${BASH_REMATCH[1]}"
-    fi
-      
     untracked=""
-    if [[ "$status" =~ "$UNTRACKED" ]] ; then
-      untracked="add"
+    if [[ $status =~ $UNTRACKED_PATTERN ]]; then
+      untracked=$EXTERNAL #"add"
     fi
 
-    if [[ "$status" =~ "$CHANGED_PATTERN" ]] ; then
-      message=`build_git_suggestion "$untracked" "commit"`
-      pscmd="[${BLUE_B}git:$project:$branch$message$RESET]> "
+    color="$RESET"
 
-    elif [[ "$status" =~ "$MODIFIED_PATTERN" ]] ; then
-      message=`build_git_suggestion "$untracked" "commit"`
-      pscmd="[${BLUE_B}git:$project:$branch$message$RESET]> "
-
-    elif [[ "$status" =~ "$NEW_FILE_PATTERN" ]] ; then
-      message=`build_git_suggestion "$untracked" "commit"`
-      pscmd="[${BLUE_B}git:$project:$branch$message$RESET]> "
-
+    if [[ $status =~ $CHANGED_PATTERN ]]; then
+      message=`build_git_suggestion "$untracked" "$MODIFIED"` #"commit"`
+      color="$BLUE_B" 
+    elif [[ $status =~ $MODIFIED_PATTERN ]]; then
+      message=`build_git_suggestion "$untracked" "$MODIFIED"` #"commit"`
+      color="$BLUE_B"
+    elif [[ $status =~ $NEW_FILE_PATTERN ]]; then
+      message=`build_git_suggestion "$untracked" "$MODIFIED"` #"commit"`
+      color="$BLUE_B"
     else
-      # todo: refactor this into a function
-      git_upstream_diff=`git diff origin/master > /dev/null`
 
-      if [[ "$git_upstream_diff" =~ "fatal: " ]] ; then
-        message=`build_git_suggestion "$untracked" "remote setup?"`
-        pscmd="[${YELLOW_B}git:$project:$branch$message$RESET]> "
-      elif [[ "$git_upstream_diff" != "" ]] ; then
-        message=`build_git_suggestion "$untracked" "merge"`
-        pscmd="[${PURPLE_B}git:$project:$branch$message$RESET]> "
+      merge_remote="false"
+      has_remote=`git remote -v`
+
+      if [[ $has_remote != "" ]]; then
+        diff_remote=`git diff origin/master`
+        if [[ $diff_remote != "" ]]; then
+          push_rebase_merge="true"
+        fi
+      fi
+ 
+      if [[ merge_remote == "true" ]]; then
+        message=`build_git_suggestion "$untracked" "$MERGE"`
+        color="$PURPLE_B"
       else
         message=`build_git_suggestion "$untracked" ""`
-        pscmd="[${BOLD_GREEN}git:$project:$branch$message$RESET]$ "
+        color="$GREEN_B"
       fi
     fi
+
+    pscmd="[${color}git@$project:$branch$message$RESET]> "
   fi
 
   export PS1="$pscmd"
@@ -128,35 +154,3 @@ function git_simple_prompt() {
 # .bashrc PROMPT_COMMAND relies on this
 export -f git_simple_prompt
 
-#
-# Whether or not to show the repo name in the git prompt
-# expects path $HOME/bash/bash-help/
-function git_prompt_toggle_repo() {
-
-  cwd="$HOME/bash/bash-help/git-simple-prompt.sh"
-  if [[ "$SHOW_REPO" == "true" ]]; then
-    echo "Setting SHOW_REPO to 'false'"
-    sed 's/SHOW_REPO=\"true\"/SHOW_REPO=\"false\"/' -i $cwd
-  else
-    echo "Setting SHOW_REPO to 'true'"
-    sed 's/SHOW_REPO=\"false\"/SHOW_REPO=\"true\"/' -i $cwd
-  fi
-
-  source $HOME/.bashrc
-}
-
-#
-# Whether or not to show suggested git actions
-function git_prompt_toggle_suggestions() {
-
-  cwd="$HOME/bash/bash-help/git-simple-prompt.sh"
-  if [[ "$SHOW_SUGGESTION" == "true" ]]; then
-    echo "Setting SHOW_SUGGESTION to 'false'"
-    sed 's/SHOW_SUGGESTION=\"true\"/SHOW_SUGGESTION=\"false\"/' -i $cwd
-  else
-    echo "Setting SHOW_SUGGESTION to 'true'"
-    sed 's/SHOW_SUGGESTION=\"false\"/SHOW_SUGGESTION=\"true\"/' -i $cwd
-  fi
-
-  source $HOME/.bashrc
-}
