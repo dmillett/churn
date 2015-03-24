b9e0e327324ce7222d0e4e76fc7b5085e7dafb8f
dmillett <dbmillett@gmail.com>
2015-03-24 09:54:51 -0500

    R line plot for files vs net

--- churn-tools.sh
+++ churn-tools.sh
@@ -2,73 +2,80 @@
 
 source "git-commit-churn.sh"
 
+function __toggle() {
+  git_churn_toggle_header
+  git_churn_toggle_footer
+}
+
+function __toggle_all() {
+  git_churn_toggle_header
+  git_churn_toggle_footer
+  git_churn_toggle_total_stats
+}
+
 #
 # For all of those Confluence wiki users out there, you can
 # create a wiki table from these statistics to paste as 'markup insert'
 function churn_to_confluence_table() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+  __toggle
   echo "{table-plus}"
   print_header "true" | sed 's/|/||/g' | sed '/=/d'
   git_churn "$@"
   echo "{table-plus}"
+  __toggle
 }
 
 function churn_dates_to_confluence_table() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+__toggle
   echo "{table-plus}"
   print_date_header "true" | sed 's/|/||/g' | sed '/=/d'
   git_churn "$@"
   echo "{table-plus}"
+  __toggle
 }
 
 function churn_messages_to_confluence_table() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+  __toggle
   echo "{table-plus}"
   print_commit_msg_header "true" | sed 's/|/||/g' | sed '/=/d'
   git_churn "$@"
   echo "{table-plus}"
+  __toggle
 }
 
 #
 # Use for R or other plotting framework
 function churn_to_csv() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+  __toggle_all
   print_header "true" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/' | sed '/=/d'
   git_churn "$@" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/'
+  __toggle_all
 }
 
 function churn_dates_to_csv() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+  __toggle_all
   print_date_header "true" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/' | sed '/=/d'
   git_churn_dates "$@" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/'
+  __toggle_all
 }
 
 function churn_messages_to_csv() {
-
-  git_churn_toggle_footer
-  git_churn_toggle_header
+  __toggle_all
   print_commit_msg_header "true" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/' | sed '/=/d'
   git_message_churn "$@" | sed 's/|/,/g' | sed 's/^.\(.*\).$/\1/'
+  __toggle_all
 }
 
 function churn_R_pie_graph_for() {
 
   OPTIND=1
   ds="lines"
-  while getopts "d:flgsn" options; do
+  output="churn-pie"
+
+  while getopts "d:flgsno:" options; do
     case $options in
       d)
-        input="$OPTARG";
+        data="$OPTARG";
         ;;
       f)
         ds="files";
@@ -85,20 +92,24 @@ function churn_R_pie_graph_for() {
       n)
         ds="net";
         ;;
+      o)
+        output="$OPTARG"
+        ;;
       *)
         echo "Try '-d data.csv' with flags -f (files), -l (lines)"
         ;;
     esac
   done
 
-  csvfile="$input"
-  rfile="$(pwd)/pie_line_dates.r"
+  csvfile="$data"
+  rfile="$(pwd)/$output.r"
   echo "#!/usr/bin/Rscript  --vanilla --default-packages=utils" > $rfile
   echo "data <- read.csv(\"$csvfile\")" >> $rfile
   echo "data_source <- data\$$ds" >> $rfile
   echo "dates_data <- data\$dates" >> $rfile
   echo "pie(data_source, main=\"$ds mods by date\", col=rainbow(length(data_source)), label=dates_data)" >> $rfile
 
+  # Defaults to 'pdf' output, it should be 'png'
   chmod 755 $rfile
   Rscript $rfile
 }
@@ -119,4 +130,22 @@ function churn_R_pie_graph_for() {
 #> lines(ad$net, col="orange", type="b", lwd=2)
 #> legend(min(ad$lines), max(ad$lines), c("files", "lines", "growth", "shrink", "net"), cex=0.8, col=c("black", "blue", "green", "red", "orange"), pch=21:22, lty=1:2);
 
+#
+# Where file mod count meets net growth/shrink == config files or coupled code
+function churn_plot_files_and_net() {
+
+  input="$1"
+  rfile="$(pwd)/fileplots.r"
+  echo "#!/usr/bin/Rscript  --vanilla --default-packages=utils" > $rfile
+  echo "d <- read.csv(\"$input\")" >> $rfile
+  echo "om <- par(mar = c(10,4,5,2) + 0.1)" >> $rfile
+  echo "plot(d\$files,type=\"l\",ylim=c(min(d\$shrink),max(d\$lines)),lwd=2,xaxt=\"n\",col=\"black\",ylab=\"# mods\",xlab=\"\")" >> $rfile
+  echo "axis(1,at=1:length(d\$filename),labels=d\$filename,las=2)" >> $rfile
+  echo "par(om)" >> $rfile
+  echo "lines(d\$net, col=\"orange\", type=\"l\", lwd=2)" >> $rfile
+
+  # Defaults to 'pdf' output, it should be 'png'
+  chmod 755 $rfile
+  Rscript $rfile
+}
 
